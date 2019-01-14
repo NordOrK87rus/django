e3c319284413273b74e9a6a4d800ee2850e4bbcc
nordork <nordork@gmail.com>
2019-01-14 15:01:46 +0300

    lesson5_hw: fix 8 (css for cart ready)

--- eshop/products/views.py
+++ eshop/products/views.py
@@ -3,20 +3,36 @@ from .models import Product, Category
 import json
 
 
+def get_products(cat_pk=None):
+
+    pl = get_list_or_404(Product.objects.all())
+    category = None
+    if cat_pk:
+        if cat_pk == 0:
+            pl = get_list_or_404(Product.objects.all().order_by('name'))
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(Category, pk=cat_pk)
+            pl = get_list_or_404(Product.objects.filter(category__pk=cat_pk).order_by('name'))
+
+    pl_arr = []
+    for i in range(0, 12, 3):
+        _tmp = pl[i:i+3]
+
+        if len(_tmp) > 0 and len(_tmp) % 3 != 0:
+            pl_arr.append(_tmp + [None]*(3-len(_tmp) % 3))
+        else:
+            pl_arr.append(_tmp)
+
+    return pl_arr, category
+
+
 def products_view(request, pk=None):
     with open('menu.json', 'r') as jf:
         menu_data = json.load(jf)
 
     categories = Category.objects.all()
-    pl = get_list_or_404(Product.objects.all())
-    category = None
-    if pk:
-        if pk == 0:
-            pl = get_list_or_404(Product.objects.all().order_by('name'))
-            category = {'name': 'все'}
-        else:
-            category = get_object_or_404(Category, pk=pk)
-            pl = get_list_or_404(Product.objects.filter(category__pk=pk).order_by('name'))
+    pl, category = get_products(pk)
 
     return render(
         request,
@@ -25,7 +41,7 @@ def products_view(request, pk=None):
             'menu_items': menu_data,
             'category': category,
             'categories': categories,
-            'products': [pl[i:i+3] for i in range(0, 12, 3)],
+            'products': pl,
             'promo': get_list_or_404(Product.objects.filter(is_promo=True))
         }
     )
