c222c1497dbb637ae868f894a91be9ab7a501fa2
nordork <nordork@gmail.com>
2019-01-14 12:06:43 +0300

    lesson5_hw: fix 7 (categories ready)

--- eshop/products/views.py
+++ eshop/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Product, Category
 import json
 
@@ -15,7 +15,7 @@ def products_view(request, pk=None):
             pl = get_list_or_404(Product.objects.all().order_by('name'))
             category = {'name': 'все'}
         else:
-            category = get_list_or_404(Category, pk=pk)
+            category = get_object_or_404(Category, pk=pk)
             pl = get_list_or_404(Product.objects.filter(category__pk=pk).order_by('name'))
 
     return render(
